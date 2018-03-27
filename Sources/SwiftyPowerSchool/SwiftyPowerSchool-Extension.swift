ff999f0057557f6510bea156228fb88768c681fe
CooperEdmunds <cooper6789@gmail.com>
2018-03-27 15:08:49 -0400

    Implement client
    This is the initial implementation of the SwiftyPowerSchool client
    for interacting with the PowerSchool API.

---
+++ Sources/SwiftyPowerSchool/SwiftyPowerSchool-Extension.swift
@@ -0,0 +1,61 @@
+//
+//    SwiftyPowerSchool-Extension.swift
+//
+//    Copyright (c) 2018 Cooper Edmunds & Doug Penny – North Raleigh Christian Academy
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+
+
+import Foundation
+
+extension SwiftyPowerSchool {
+    public func getSchools(completion: @escaping ([School]?, Error?) -> ()) {
+        let path = "/ws/v1/district/school"
+        fetchData(path: path, model: Schools.self, method: "GET") {schoolsObj, error in
+            let schools = schoolsObj?.data
+            completion(schools, error)
+        }
+    }
+    
+    public func getCoursesFromSchool(_ schoolID: Int, completion: @escaping ([Course]?, Error?) -> ()) {
+        let path = "/ws/v1/school/\(schoolID)/course"
+        fetchData(path: path, model: Courses.self, method: "GET") {coursesObj, error in
+            let courses = coursesObj?.data
+            completion(courses, error)
+        }
+    }
+    
+    public func getSectionsFromSchool(_ schoolID: Int, completion: @escaping ([Section]?, Error?) -> ()) {
+        let path = "/ws/v1/school/\(schoolID)/section"
+        fetchData(path: path, model: Sections.self, method: "GET") {sectionsObj, error in
+            let sections = sectionsObj?.data
+            completion(sections, error)
+        }
+    }
+    
+    public func getSchoolsCount(completion: @escaping (Int?, Error?) -> ()) {
+        let path = "/ws/v1/district/school/count"
+        fetchData(path: path, model: ResourceCount.self, method: "GET") {resourceCount, error in
+            let count = resourceCount?.count
+            completion(count, error)
+        }
+    }
+}
+
+
--- Sources/SwiftyPowerSchool/SwiftyPowerSchool.swift
+++ Sources/SwiftyPowerSchool/SwiftyPowerSchool.swift
@@ -21,3 +21,128 @@
 //    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //    SOFTWARE.
 
+
+import Foundation
+
+public class SwiftyPowerSchool {
+    let baseURL: URL?
+    let clientID: String
+    let clientSecret: String
+    var token: Token?
+    var metadata: Metadata?
+    
+    public init(_ baseURL: String, clientID: String, clientSecret: String) {
+        self.baseURL = URL(string: baseURL)
+        self.clientID = clientID
+        self.clientSecret = clientSecret
+    }
+    
+    func fetchData<Model:Codable>(path: String, model: Model.Type, method: String, completion: @escaping (Model?, Error?) -> ()) {
+        clientURLRequest(path: path, completion: { request, error in
+            if let request = request {
+                self.dataTask(request: request, method: method) { data, error in
+                    if let data = data {
+                        let decoder = JSONDecoder()
+                        do {
+                            let object = try decoder.decode(model.self, from: data)
+                            completion(object, nil)
+                        }
+                        catch let parseError {
+                            completion(nil, parseError)
+                        }
+                    }
+                    else {
+                        completion(nil, error)
+                    }
+                }
+            }
+            else {
+                completion(nil, error)
+            }
+        })
+    }
+    
+    private func requestAuthToken(completion: @escaping (Bool, Error?) -> ()) {
+        let concatCreds = self.clientID + ":" + self.clientSecret
+        guard let utf8Creds = concatCreds.data(using: .utf8) else {
+            completion(false, nil)
+            return
+        }
+        let base64Creds = utf8Creds.base64EncodedString()
+        var request = URLRequest(url: URL(string: "/oauth/access_token/", relativeTo: self.baseURL)!)
+        request.setValue("Basic " + base64Creds, forHTTPHeaderField: "Authorization")
+        request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = "grant_type=client_credentials".data(using: .utf8)!
+        dataTask(request: request, method: "POST") { tokenData, error in
+            if let tokenData = tokenData {
+                let decoder = JSONDecoder()
+                do {
+                    let authToken = try decoder.decode(Token.self, from: tokenData)
+                    self.token = authToken
+                    completion(true, nil)
+                }
+                catch let parseError {
+                    completion(false, parseError)
+                }
+            }
+            else {
+                completion(false, error)
+            }
+        }
+    }
+    
+    internal func dataTask(request: URLRequest, method: String,  completion: @escaping (Data?, Error?) -> ()) {
+        var request = request
+        request.httpMethod = method
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(nil, error)
+                return
+            }
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    internal func clientURLRequest(path: String, params: Dictionary<String, AnyObject>? = nil, completion: @escaping (URLRequest?, Error?) -> ()) {
+        let requestURL = URL(string: path, relativeTo: self.baseURL)!
+        var request = URLRequest(url: requestURL)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let params = params {
+            var paramString = ""
+            for (key, value) in params {
+                let escapedKey = key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                let escapedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                if let escapedKey = escapedKey, let escapedValue = escapedValue {
+                    paramString += "\(escapedKey)=\(escapedValue)&"
+                }
+            }
+            request.httpBody = paramString.data(using: .utf8)
+        }
+        
+        if let token = self.token {
+            request.setValue(token.tokenType + " " + token.accessToken, forHTTPHeaderField: "Authorization")
+            completion(request, nil)
+        }
+        else {
+            self.requestAuthToken(completion: { success, error in
+                if success {
+                    if let token = self.token {
+                        request.setValue(token.tokenType + " " + token.accessToken, forHTTPHeaderField: "Authorization")
+                        completion(request, nil)
+                    }
+                    else {
+                        completion(nil, error)
+                    }
+                }
+                else {
+                    completion(nil, error)
+                }
+            })
+        }
+    }
+}
+
